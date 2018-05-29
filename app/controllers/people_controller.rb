b7f8fbded19ea190af6ae61e80e5aeef8a3f82bc
Brittanie Pham <brittaniepham@gmail.com>
2018-05-29 14:45:48 -0600

    edit/update and add/create person

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,6 @@
 class PeopleController < ApplicationController
   def index
-    @people = Person.all
+    @people = Person.order(updated_at: :DESC)
   end
 
   def show
@@ -12,9 +12,31 @@ class PeopleController < ApplicationController
   end
 
   def create
-    
+    @person = Person.new(person_params)
+
+    if @person.save
+      redirect_to person_path(@person.id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @person = Person.find(params[:id])
+  end
+
+  def update
+    @person = Person.find(params[:id])
+
+    if @person.update(person_params)
+      redirect_to person_path(@person.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def person_params
+    params.require(:person).permit(:name, :age, :hair_color, :eye_color, :gender, :alive)
   end
 end
